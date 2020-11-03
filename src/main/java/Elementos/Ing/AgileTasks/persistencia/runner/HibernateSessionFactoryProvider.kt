ff3e43bec0f1f15fddc9085b16270abf7d5c377d
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2020-11-02 23:58:57 -0300

    Update HibernateSessionFactoryProvider.kt

--- src/main/java/Elementos/Ing/AgileTasks/persistencia/runner/HibernateSessionFactoryProvider.kt
+++ src/main/java/Elementos/Ing/AgileTasks/persistencia/runner/HibernateSessionFactoryProvider.kt
@@ -10,17 +10,22 @@ class HibernateSessionFactoryProvider private constructor() {
 
     init {
         val env = System.getenv()
-        val user = env.getOrDefault("USER", "root")
-        val password = env.getOrDefault("PASSWORD", "root")
-        val dataBase = env.getOrDefault("DATA_BASE", "AgileTasks_DB")
-        val host = env.getOrDefault("HOST", "localhost")
+        val user = env.getOrDefault("MYSQL_USERNAME", "root")
+        val password = env.getOrDefault("MYSQL_PASSWORD","root")
+        val host = "localhost"
+        val dataBase = env.getOrDefault("MYSQL_DATABASE", "Agiletasks_DB")
 
+        val url = env.getOrDefault("SQL_URL", "jdbc:mysql://$host:3306/$dataBase?createDatabaseIfNotExist=true&serverTimezone=UTC")
+        val dialect = env.getOrDefault("HIBERNATE_DIALECT", "org.hibernate.dialect.MySQL8Dialect")
+        val driver = env.getOrDefault("SQL_DRIVER", "com.mysql.cj.jdbc.Driver")
 
         val configuration = Configuration()
         configuration.configure("hibernate.cfg.xml")
         configuration.setProperty("hibernate.connection.username", user)
         configuration.setProperty("hibernate.connection.password", password)
-        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://$host:3306/$dataBase?createDatabaseIfNotExist=true&serverTimezone=UTC")
+        configuration.setProperty("hibernate.connection.url", url)
+        configuration.setProperty("connection.driver_class", driver)
+        configuration.setProperty("hibernate.dialect", dialect)
         this.sessionFactory = configuration.buildSessionFactory()
     }
 
