6904abd6fbf1eb4d7ea3c7caebecd86d896a88e2
ignacio mendez <ignacio.mendez0000@gmail.com>
2020-11-02 22:19:18 -0300

    hola

---
+++ src/main/java/Elementos/Ing/AgileTasks/modelo/Nota.java
@@ -0,0 +1,41 @@
+package Elementos.Ing.AgileTasks.modelo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="notas")
+public class Nota {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="titulo")
+    private String titulo;
+    @Column(name="descr")
+    private String descripcion;
+    @ManyToOne(optional = false)
+    private Usuario user;
+    //private String task;
+
+    public Nota(){}
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescripcion(String descrpicion) {
+        this.descripcion = descrpicion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescrpicion() {
+        return descripcion;
+    }
+
+    public void setUser(Usuario user) {this.user = user;}
+
+    public int getId() { return id; }
+}
